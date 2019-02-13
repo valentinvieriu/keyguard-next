8374d7d8dd95a42e80d04e39a965c62772644d1a
sisou <hello@soerenschwert.de>
2019-02-12 21:41:10 -0600

    Lock Nimiq lib version to 1.4.1

--- karma.conf.js
+++ karma.conf.js
@@ -14,7 +14,7 @@ module.exports = function (/** @type {any} */ config) {
 
         // list of files / patterns to load in the browser
         files: [
-            'https://cdn.nimiq-testnet.com/web-offline.js',
+            'https://cdn.nimiq-testnet.com/v1.4.1/web-offline.js',
             'src/**/*js',
             'tests/**/*.spec.js',
             { 'pattern': 'src/assets/Iqons.min.svg', 'watched': false, 'included': false, 'served': true },
